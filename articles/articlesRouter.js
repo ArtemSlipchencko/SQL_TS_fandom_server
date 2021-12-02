2cfc41ac0222563828c795ce4c9e42dc0a72a072
ArtemSlipchencko <a.slipchencko@gmail.com>
2021-12-02 20:43:49 +0200

    fixes

--- articles/articlesRouter.js
+++ articles/articlesRouter.js
@@ -1,10 +1,24 @@
 const { Router } = require("express");
-const { authorization } = require("./../middlewares/middlewares");
+const { authorization, checkRights } = require("./../middlewares/middlewares");
 const { createArticle, removeArticle } = require("./articlesController");
 
 const articlesRouter = Router();
 
-articlesRouter.post("/article", authorization, createArticle);
-articlesRouter.delete("/article", authorization, removeArticle);
+articlesRouter.post(
+  "/article",
+  authorization,
+  (req, res, next) => {
+    checkRights(req, res, next, "admin moder author");
+  },
+  createArticle,
+);
+articlesRouter.delete(
+  "/article",
+  authorization,
+  (req, res, next) => {
+    checkRights(req, res, next, "admin moder author");
+  },
+  removeArticle,
+);
 
 module.exports = articlesRouter;
